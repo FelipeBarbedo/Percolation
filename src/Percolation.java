c07b01c99dd6c530b7e485ec108bb732a767483b
Felipe Barbedo <felipebarbedo54@gmail.com>
2024-07-24 11:01:11 -0300

    Updating percolation problem.

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,15 +1,16 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-import edu.princeton.cs.algs4.StdOut;
 
 public class Percolation {
 
     private WeightedQuickUnionUF gridConnectionTree;
     private int[][] grid;
+    private int n;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
         gridConnectionTree = new WeightedQuickUnionUF((n * n) + 2);
         grid = new int[n][n];
+        this.n = n;
 
         for (int row = 0; row < grid.length; row++)
             for (int col = 0; col < grid[row].length; col++)
@@ -18,7 +19,37 @@ public class Percolation {
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
-        grid[row][col] = 0;
+        boolean up = false, down = false;
+        boolean left = false, right = false;
+
+        if (row > 0)
+            up = this.isFull(row - 1, col);
+
+        if (row < n)
+            down = this.isFull(row + 1, col);
+
+        if (col > 0)
+            left = this.isFull(row, col - 1);
+
+        if (col < n)
+            right = this.isFull(row, col + 1);
+
+        if (row == 0)
+            gridConnectionTree.union(col, this.n + 1);
+
+        if (up) {
+            gridConnectionTree.union(row * (n + 1) + col, (row - 1) * (n + 1) + col);
+            grid[row][col] = 1;
+        } else if (down) {
+            gridConnectionTree.union(row * (n + 1) + col, (row + 1) * (n + 1) + col);
+            grid[row][col] = 1;
+        } else if (left) {
+            gridConnectionTree.union(row * (n + 1) + col, row * (n + 1) + (col - 1));
+            grid[row][col] = 1;
+        } else if (right) {
+            gridConnectionTree.union(row * (n + 1) + col, row * (n + 1) + (col + 1));
+            grid[row][col] = 1;
+        }
     }
 
     // is the site (row, col) open?
@@ -45,14 +76,6 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates() {
-        for (int col = 0; col < grid.length; col++)
-            if (this.isFull(grid.length - 1, col))
-                return true;
-
-        return false;
-    }
-
-    public static void main(String[] args) {
-        StdOut.printf("Hello");
+        return gridConnectionTree.find(this.n + 1) == gridConnectionTree.find(this.n + 2);
     }
 }
